ca37f9e4667ee0e2ae0d872d20aa98884f2ff83a
Nyokong <50559078+Nyokong@users.noreply.github.com>
2024-09-21 17:36:55 +0200

    fixed the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,11 +4,6 @@
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-
-// components
-import Header from '@/components/header';
-
-// axios
 import axios from 'axios';
 
 import { refreshToken } from './utils/auth';
@@ -16,12 +11,11 @@ import { refreshToken } from './utils/auth';
 // import for cookies
 import Cookies from 'js-cookie';
 
-export default function Home() {
+function header() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [user, setUser] = useState(null);
     const router = useRouter();
 
-    // use effect to check if the user is logged in
     useEffect(() => {
         const loggedIn = Cookies.get('access_token');
         if (loggedIn) {
@@ -68,12 +62,36 @@ export default function Home() {
             router.push('/');
         }
     }, []);
+
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <Header />
-            <h1 className="">Home Page</h1>
+        <div className="w-full h-[100px] flex flex-row">
+            <div className="flex flex-row justify-end">
+                {isLoggedIn ? (
+                    <div>
+                        {user ? (
+                            <div>
+                                <h1>Welcome back, {user.first_name}!</h1>
+                                {/* <p>Email: {user.email}</p> */}
+                            </div>
+                        ) : (
+                            <div>no user data...</div>
+                        )}
+                    </div>
+                ) : (
+                    <div>
+                        Please log in to see this content.
+                        <div
+                            className="flex flex-row justify-center 
+                            w-[200px] h-full rounded-lg bg-purple-700 text-white
+                        "
+                        >
+                            <Link href="/login">Login here</Link>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
 
-
+export default header;
